65d8b4c96dc2ea183733bd40e0b36d4a79db5c7a
lq-jw <83740897+lq-jw@users.noreply.github.com>
2021-11-02 11:43:13 +0800

    Add files via upload

---
+++ HW-1.ino
@@ -0,0 +1,24 @@
+// For a connection via I2C using the Arduino Wire include:
+#include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
+#include "SSD1306Wire.h"        // legacy: #include "SSD1306.h"
+
+SSD1306Wire display(0x3c, 21, 22);   
+
+
+void setup() {
+  Serial.begin(115200);
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_24);
+  pinMode(13, INPUT);
+}
+
+void loop() {
+  display.clear();
+  int input = analogRead(13);
+  String myString = String(input);  
+  display.drawString(0, 0, "Value");
+  display.drawString(0, 30, myString);
+  display.display();
+  Serial.println(input);
+}
